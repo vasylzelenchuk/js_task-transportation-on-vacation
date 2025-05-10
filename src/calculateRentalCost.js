3f837a3f51cc077c2a102a2ff7468073e76ae286
vasylzelenchuk <vasyazyasinya@gmail.com>
2025-05-10 18:40:28 +0200

    add task soulition

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,28 @@
  *
  * @return {number}
  */
+const basePrice = 40;
+
+const BASIC_TERM = 3;
+const BASIC_TERM_DISCOUNT = 20;
+
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days === LONG_TERM) {
+    return basePrice * days - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= BASIC_TERM) {
+    return basePrice * days - BASIC_TERM_DISCOUNT;
+  }
+
+  if (days < BASIC_TERM) {
+    return basePrice * days;
+  }
+
+  return 40;
 }
 
 module.exports = calculateRentalCost;
